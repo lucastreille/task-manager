ee4d58a8786b21b2870627a505e1eb25313c1854
GabrielC0 <gabriel.changrenier@icloud.com>
2026-01-21 16:23:35 +0100

    Partie 5 : Administration (Bonus Expert)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
-import { authGuard } from './core/auth/guards/auth.guard';
-
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { TasksListComponent } from './features/tasks/pages/tasks-list/tasks-list.component';
 import { TaskFormComponent } from './features/tasks/pages/task-form/task-form.component';
 import { TaskDetailComponent } from './features/tasks/pages/task-detail/task-detail.component';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { UsersListComponent } from './features/users/users-list/users-list.component';
+import { authGuard } from './core/auth/guards/auth.guard';
+import { adminGuard } from './core/auth/guards/admin.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -17,8 +19,9 @@ export const routes: Routes = [
   { path: 'tasks', component: TasksListComponent, canActivate: [authGuard] },
   { path: 'tasks/new', component: TaskFormComponent, canActivate: [authGuard] },
   { path: 'tasks/:id/edit', component: TaskFormComponent, canActivate: [authGuard] },
-
   { path: 'tasks/:id', component: TaskDetailComponent, canActivate: [authGuard] },
+
+  { path: 'users', component: UsersListComponent, canActivate: [authGuard, adminGuard] },
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' },
